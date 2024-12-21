91aebb27620fa1b4f5ad7a487298ae451edb7644
vksakpal <vinay.sakpal@gmail.com>
2024-12-21 17:07:03 +0530

    Add project files.

---
+++ DataAccessWrapper.Sql/IDataAccessFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace DataAccessWrapper.Sql
+{
+
+    /// <summary>
+    /// Data Access Factory Interface
+    /// </summary>
+    public interface IDataAccessFactory
+    {
+        /// <summary>
+        /// Returns DbRepository Interface for single database
+        /// </summary>
+        /// <param name="dbAlias"></param>
+        /// <returns></returns>
+        IDbRepository GetRepository(string dbAlias);
+
+        /// <summary>
+        /// Returns DbRepository Interface for primary and secondary database
+        /// </summary>
+        /// <param name="primaryDbAlias"></param>
+        /// <param name="secondaryDbAlias"></param>
+        /// <remarks>If primary db execution fails, it will try to execute the secondary database</remarks>
+        /// <returns></returns>
+
+        IDbRepository GetDbRepository(string primaryDbAlias, string secondaryDbAlias);
+    }
+
+    public class DataAccessRepository : IDataAccessFactory
+    {        
+        public IDbRepository GetDbRepository(string primaryDbAlias, string secondaryDbAlias)
+        {
+            return new DbRepository(primaryDbAlias, secondaryDbAlias);
+        }
+
+        public IDbRepository GetRepository(string dbAlias)
+        {
+            return new DbRepository(dbAlias);
+        }
+    }
+
+}
---
+++ DataAccessWrapper.Sql/IDbRepository.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace DataAccessWrapper.Sql
+{
+    public interface IDbRepository
+    {       
+        /// <summary>
+        /// Executes the stored procedure and returns the dataset
+        /// </summary>
+        /// <param name="storedProcedureName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="outputTableNames"></param>
+        /// <returns></returns>
+        DataSet ExecuteAsync(string storedProcedureName, DbParameter[] parameters, List<string> outputTableNames);
+
+        /// <summary>
+        /// Executes the stored procedure and returns the dataset
+        /// </summary>
+        /// <param name="storedProcedureName"></param>
+        /// <param name="outputTableNames"></param>
+        /// <returns></returns>
+        DataSet ExecuteAsync(string storedProcedureName, List<string> outputTableNames);
+    }
+
+    public class DbRepository : IDbRepository
+    {
+        private readonly string _executionStrategy;
+
+        private readonly string _primaryDbAlias;
+
+        private readonly string _secondaryDbAlias;
+
+        public DbRepository(string dbAlias)
+        {
+            _primaryDbAlias = dbAlias;
+            _executionStrategy = "STANDALONE";
+            _secondaryDbAlias = string.Empty;
+        }
+
+
+        public DbRepository(string primaryDbAlias, string secondaryDbAlias)
+        {
+            _executionStrategy = "HOTCOLD";
+            _primaryDbAlias = primaryDbAlias;
+            _secondaryDbAlias = secondaryDbAlias;
+        }
+
+
+
+        public DataSet ExecuteAsync(string storedProcedureName, DbParameter[] parameters, List<string> outputTableNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataSet ExecuteAsync(string storedProcedureName, List<string> outputTableNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        DataSet IDbRepository.ExecuteAsync(string storedProcedureName, DbParameter[] parameters, List<string> outputTableNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        DataSet IDbRepository.ExecuteAsync(string storedProcedureName, List<string> outputTableNames)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
